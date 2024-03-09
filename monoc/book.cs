86965aa5906b13ff63fb4ea2de75ebf216152eac
Harry Collins <sensorydog@gmail.com>
2024-03-09 20:21:19 +1000

    Thats a lota stuff

---
+++ monoc/book.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace monoc
+{
+    public partial class book : Form
+    {
+        public book()
+        {
+            InitializeComponent();
+        }
+    }
+}
